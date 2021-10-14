927ac5512e70881e197cedd5e5a2619bf9f8a7dd
zlegnain <zaklegnain@cmail.carleton.ca>
2021-10-14 15:55:09 -0700

    test

---
+++ src/Node.java
@@ -0,0 +1,13 @@
+/** Node class that represents a pixel in mxn grid
+ * @author Zakariya Legnain
+ * @version 1.0, 10/14/2021
+ */
+
+
+public class Node {
+
+    private int x;
+    private int y;
+
+
+}
