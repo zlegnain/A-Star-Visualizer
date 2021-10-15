fb7afb8ea36e09d48925f603235af5d910b59efe
zlegnain <zaklegnain@cmail.carleton.ca>
2021-10-14 20:22:13 -0700

    added class variables

--- src/Astar.java
+++ src/Astar.java
@@ -1,8 +1,27 @@
+import java.util.*;
+
+
+
 /**
- *
+ * Astar class
+ * It will handle initializing nodes in the grid, finding neighbours of currnode
+ * finding the shortest path from the start-state to goal-state
+ * @author Zakariya Legnain
  */
 
-
 public class Astar {
+
+    // start and goal nodes
+    private Node start;
+    private Node goal;
+
+    // frontier and explored
+    private PriorityQueue<Node> frontier;
+    private ArrayList<Node> explored;
+
+    
+
+
+
 
 }
